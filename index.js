e99112616f04069fc98db0ffac266c07d6d1f0aa
GitErwan <ik012108agi@plaiaundi.net>
2018-03-23 14:11:23 +0100

    3.0
    1. He añadido búsqueda por tamaños. 2. Version API terminada

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ app.get('/:raza', (req, res) => {
     //con "req.params.raza" coge el parámetro que se mana por la url por ejemplo: localhost:3000/pastorAleman, devuelve "pastorAleman"
     //"_.find" busca en el array razas el json que tenga el nombre que le indicas, por ejemplo Pasotor Aleman, y te devuelve el json con todos los datos
     //"razasJson.razas" importa el array con todas las razas
-    var bRaza = _.find(razasJson.razas, {castellano: req.params.raza}).ingles
+    var bRaza = _.find(razasJson.razas, { castellano: req.params.raza }).ingles
     foto(bRaza)
         .then((resp) => {
             //String
-            var perros = { 
+            var perros = {
                 raza: req.params.raza,
                 foto: resp.message,
-                regionOrigen: _.find(razasJson.razas, {castellano: req.params.raza}).regionOrigen,
-                dimensiones: _.find(razasJson.razas, {castellano: req.params.raza}).dimensiones,
-                tamaño: _.find(razasJson.razas, {castellano: req.params.raza}).tamaño,
-                peso: _.find(razasJson.razas, {castellano: req.params.raza}).peso,
-                caracter: _.find(razasJson.razas, {castellano: req.params.raza}).caracter,
-                descripcion: _.find(razasJson.razas, {castellano: req.params.raza}).descripcion 
+                regionOrigen: _.find(razasJson.razas, { castellano: req.params.raza }).regionOrigen,
+                dimensiones: _.find(razasJson.razas, { castellano: req.params.raza }).dimensiones,
+                tamaño: _.find(razasJson.razas, { castellano: req.params.raza }).tamaño,
+                peso: _.find(razasJson.razas, { castellano: req.params.raza }).peso,
+                caracter: _.find(razasJson.razas, { castellano: req.params.raza }).caracter,
+                descripcion: _.find(razasJson.razas, { castellano: req.params.raza }).descripcion
             };
             res.send(perros);
         });
@@ -40,11 +40,26 @@ app.get('/:raza', (req, res) => {
 app.get('/lista/razas', (req, res) => {
     var i;
     var lista = [];
-    for(i=0; i<razasJson.razas.length; i++){
-        var sig = {nombre: _.find(razasJson.razas, {id: i.toString()}).castellano};
+    for (i = 0; i < razasJson.razas.length; i++) {
+        var sig = { nombre: _.find(razasJson.razas, { id: i.toString() }).castellano };
         lista.push(sig);
     }
     res.send(lista);
 })
 
+app.get('/tamano/:tamano', (req, res) => {
+    if (req.params.tamano == "Toy" || req.params.tamano == "Pequeño" || req.params.tamano == "Medio" || req.params.tamano == "Grande" || req.params.tamano == "Gigante") {
+        var i;
+        var lista = [];
+        for (i = 0; i < razasJson.razas.length; i++) {
+            if (_.find(razasJson.razas, { id: i.toString() }).tamaño == req.params.tamano) {
+                var sig = { nombre: _.find(razasJson.razas, { id: i.toString(), tamaño: req.params.tamano }).castellano };
+                lista.push(sig);
+            }
+        }
+        res.send(lista);
+    } else {
+        res.send("No existe ese tamaño");
+    }
+})
 app.listen('3000');
